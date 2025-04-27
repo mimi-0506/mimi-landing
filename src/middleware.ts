34072919e352f54a7eaab9e9457035a673acc24f
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-04-27 17:53:14 +0900

    feat: 미들웨어 위치 및 내용 수정

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log("✅ 미들웨어 작동:", request.nextUrl.pathname);
+
+  if (request.nextUrl.pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/mimimemo";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/"],
+};
