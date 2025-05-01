f81961747aa69c00fb4dafd441afc0e36176e267
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-05-01 15:55:51 +0900

    fix: subset추가

--- src/fonts.ts
+++ src/fonts.ts
@@ -3,6 +3,7 @@ import { Nanum_Myeongjo, Song_Myung } from "next/font/google";
 export const myeongjo = Nanum_Myeongjo({
   weight: ["400", "700"],
   display: "swap",
+  subsets: ["latin"],
 });
 
 export const songmyung = Song_Myung({
